8cb45f6c3cab0a6ef2f614aeab2338597e8436ea
Lemoel <lemoel@gmail.com>
2022-03-17 17:49:35 +0000

    Classes

--- src/main/kotlin/com/studykotlin/Pessoa.kt
+++ src/main/kotlin/com/studykotlin/Pessoa.kt
@@ -1,3 +1,11 @@
 package com.studykotlin
 
-class Pessoa(val nome: String, val idade: Int);
+class Pessoa(
+    val nome: String,
+    val idade: Int
+);
+
+fun main() {
+    val pessoa: Pessoa = Pessoa("Lemoel", 42);
+    val pessoa2: Pessoa = Pessoa("Lemoel", 42);
+}
